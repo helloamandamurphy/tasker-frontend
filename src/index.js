4019d03948b00e56f4de6b436c416df65b950bfb
Amanda Murphy <helloamandamurphy@gmail.com>
2020-02-28 11:05:17 -0500

    Set up index.js and create store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
 import App from './App';
 
-//set up store
+// Review compose/Redux
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Store stores the data globally
+// Reducer will return a different version of the store, based on the action
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  < Provider store = { store }>
+    < App />
+  </ Provider >
+  ,
+  document.getElementById('root'));
