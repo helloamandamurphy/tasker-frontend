c51b284fc8c3782d4c1f628118219b85998c772f
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-12 16:59:26 -0400

    Create file to render user's lists

---
+++ src/components/MyLists.js
@@ -0,0 +1,22 @@
+// React + Dependencies
+import React from 'react'
+import { connect } from 'react-redux'
+
+//Import from Files
+import ListCard from './ListCard.js'
+
+const MyLists = props => {
+  const listCards = props.lists.length > 0 ?
+    props.lists.map(l => <ListCard list={l} key={l.id}/>) : null
+  return (
+    listCards
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    lists: state.myLists
+  }
+}
+
+export default connect(mapStateToProps)(MyLists);
