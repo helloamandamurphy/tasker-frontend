78bca1d90cae36cf95edb11f1bcb79561193a3a0
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-17 17:08:58 -0400

    Add synchronous action creators for update and reset sign up form

---
+++ src/actions/signupForm.js
@@ -0,0 +1,15 @@
+// synchronous action creators
+export const updateSignupForm = (formData) => {
+  return {
+    type: "UPDATE_SIGNUP_FORM",
+    formData
+  }
+}
+
+export const resetSignupForm = () => {
+  return {
+    type: "RESET_SIGNUP_FORM"
+  }
+}
+
+// asynchronous action creators
