5a775f899cf7ad69f9031aaa3afe70c1bb085869
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-17 17:12:18 -0400

    Add route and import Signup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Route } from 'react-router-dom'
 import { getCurrentUser } from "./actions/currentUser.js"
 import NavBar from "./components/NavBar.js"
 import MainContainer from "./containers/MainContainer.js"
+import Signup from './components/Signup.js'
 import Login from './components/Login.js'
 import Logout from './components/Logout.js'
 import MyLists from './components/MyLists.js'
@@ -22,6 +23,7 @@ class App extends React.Component {
       <div className="App">
         <NavBar/>
         <Route exact path='/login' component={Login}/>
+        <Route exact path='/signup' component={Signup}/>
         <Route exact path='/my-lists' component={MyLists}/>
       </div>
     );
