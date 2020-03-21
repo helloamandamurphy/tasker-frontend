12c90015bbe8b085ef6eeb0cb9630c65dfd09b5e
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-21 16:49:13 -0400

    Import withRouter to pass along router props; import Home component; add route to render different home screens depending on whether or not a user is logged in; add mapStateToProps to pass along loggedIn prop for route logic; export App withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 //React + Dependencies
 import React from 'react';
 import { connect } from 'react-redux';
-import { Route } from 'react-router-dom'
+import { Route, withRouter } from 'react-router-dom'
 
 //Import from Files
 // import ListsContainer from './containers/ListsContainer' (not currently using)
 import { getCurrentUser } from "./actions/currentUser.js"
 import NavBar from "./components/NavBar.js"
 import MainContainer from "./containers/MainContainer.js"
+import Home from './components/Home.js'
 import Signup from './components/Signup.js'
 import Login from './components/Login.js'
 import Logout from './components/Logout.js'
@@ -19,15 +20,24 @@ class App extends React.Component {
   }
 
   render() {
+    const { loggedIn } = this.props;
     return (
       <div className="App">
         <NavBar/>
-        <Route exact path='/login' component={Login}/>
+        <Route exact path='/' render={()=> loggedIn ? <MyLists/> : <Home/> }/>
         <Route exact path='/signup' component={Signup}/>
+        <Route exact path='/login' component={Login}/>
         <Route exact path='/my-lists' component={MyLists}/>
       </div>
     );
   }
 }
 
-export default connect(null, { getCurrentUser })(App);
+//Only returns whether or not our user is logged in
+const mapStateToProps = state => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+}
+
+export default withRouter(connect(mapStateToProps, { getCurrentUser })(App));
