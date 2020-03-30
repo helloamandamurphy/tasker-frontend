ac1cfef99e9b85d7f0c2a56d62a8a255233dc911
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-30 18:29:03 -0400

    Add synch actions to clearLists and addList; add async action createList to post the input from NewListForm to the backend

--- src/actions/myLists.js
+++ src/actions/myLists.js
@@ -6,6 +6,19 @@ export const setMyLists = lists => {
   }
 }
 
+export const clearLists = () => {
+  return {
+    type: "CLEAR_LISTS"
+  }
+}
+
+export const addList = list => {
+  return {
+    type: "ADD_LIST",
+    list
+  }
+}
+
 //async actions
 export const getMyLists = () => {
   return dispatch => {
@@ -25,6 +38,27 @@ export const getMyLists = () => {
         dispatch(setMyLists(response))
       }
     })
+    .catch(console.log)
+  }
+}
+
+export const createList = listData => {
+  return dispatch => {
+    const sendableListData = {
+      name: listData.name,
+      end_time: listData.endTime,
+      user_id: listData.userId
+    }
+    return fetch("http://localhost:3000/api/v1/lists", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(sendableListData)
+    })
+    .then(r => r.json())
+    .then(console.log)
     .catch(console.log)
   }
 }
