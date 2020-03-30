f3ceeb17c6cf240c62382c6de201c0ec8223bf63
Amanda Murphy <helloamandamurphy@gmail.com>
2020-03-30 18:31:28 -0400

    Set initial state, add actions for ADD_LIST and CLEAR_LISTS

--- src/reducers/myLists.js
+++ src/reducers/myLists.js
@@ -1,7 +1,13 @@
+const initialState = []
+
 export default (state = [], action) => {
   switch (action.type) {
     case "SET_MY_LISTS":
       return action.lists
+    case "ADD_LIST":
+      return state.trips.concat(action.trip)
+    case "CLEAR_LISTS":
+      return initialState
     default:
       return state
   }
