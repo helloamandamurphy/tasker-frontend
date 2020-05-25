b3d1c157953e3ac665c126d62f95cbde6c9a7ad1
Amanda Murphy <helloamandamurphy@gmail.com>
2020-05-25 14:43:55 -0400

    Import Link from react-router-dom

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,5 +1,6 @@
 // React + Dependencies
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ListCard = ({ list }) => {
   return (
@@ -7,6 +8,7 @@ const ListCard = ({ list }) => {
       <div>
         <h3>{list.name}</h3>
         <p>{list.end_time}</p>
+        <Link to={`/lists/${list.id}/edit`}>Edit this List</Link>
       </div> :
     <p>Sorry! We couldn't find your list.</p>
   )
