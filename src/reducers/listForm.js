0d84d8ae2acb4d3dad25a88cdae983ad623c5dcb
Amanda Murphy <helloamandamurphy@gmail.com>
2020-05-25 15:04:20 -0400

    Rename newListForm.js to listForm.js; add a SET_FORM_DATA_FOR_EDIT case to populate edit form with existing list information

--- src/reducers/listForm.js
+++ src/reducers/listForm.js
@@ -5,13 +5,15 @@ const initialState = {
 
 export default (state=initialState, action) => {
   switch (action.type) {
-    case "UPDATE_NEW_LIST_FORM":
+    case "UPDATE_LIST_FORM":
       return {
         ...state,
         [action.formData.name]: action.formData.value
       }
     case "RESET_NEW_LIST_FORM":
       return initialState
+    case "SET_FORM_DATA_FOR_EDIT":
+      return action.listFormData
     default:
       return state
   }
