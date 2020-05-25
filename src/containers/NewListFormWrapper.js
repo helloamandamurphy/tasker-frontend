9ed08bb317b7101d0b0c0fa235fa31f997508cfa
Amanda Murphy <helloamandamurphy@gmail.com>
2020-05-25 15:10:05 -0400

    Created a wrapper to handle the ListForm when creating a new list

---
+++ src/containers/NewListFormWrapper.js
@@ -0,0 +1,22 @@
+// React + Dependencies
+import React from 'react';
+import { connect } from 'react-redux';
+
+// Import from Files
+import { createList } from '../actions/myLists'
+import ListForm from '../components/ListForm'
+
+//Stateless Component
+const NewListFormWrapper = ({ history, createList }) => {
+
+  const handleSubmit = (formData, userId) => {
+    createList({
+      ...formData,
+      userId
+    }, history)
+  }
+
+  return <ListForm history={history} handleSubmit={handleSubmit} />
+};
+
+export default connect(null, { createList })(NewListFormWrapper);
