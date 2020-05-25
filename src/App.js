fa9375b6f17cff8dbc295d91282d84617de12e30
Amanda Murphy <helloamandamurphy@gmail.com>
2020-05-25 15:08:06 -0400

    Change imports and references from NewListForm to ListForm; import NewListFormWrapper and EditListFormWrapper; create route for editing lists, and render the EditListFormWrapper with props for the selected list

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { Route, Switch, withRouter, Link } from 'react-router-dom'
 
 //Import from Files
-// import ListsContainer from './containers/ListsContainer' (not currently using)
 import { getCurrentUser } from "./actions/currentUser"
 import NavBar from "./components/NavBar"
 import MainContainer from "./containers/MainContainer"
@@ -12,9 +11,11 @@ import Home from './components/Home'
 import Signup from './components/Signup'
 import Login from './components/Login'
 import Logout from './components/Logout'
-import NewListForm from './components/NewListForm'
+import ListForm from './components/ListForm'
 import MyLists from './components/MyLists'
 import ListCard from './components/ListCard'
+import NewListFormWrapper from './containers/NewListFormWrapper'
+import EditListFormWrapper from './containers/EditListFormWrapper'
 
 class App extends React.Component {
   componentDidMount() {
@@ -31,10 +32,14 @@ class App extends React.Component {
           <Route exact path='/signup' component={Signup}/>
           <Route exact path='/login' component={Login}/>
           <Route exact path='/lists' component={MyLists}/>
-          <Route exact path='/lists/new' component={NewListForm}/>
+          <Route exact path='/lists/new' component={NewListFormWrapper}/>
           <Route exact path='/lists/:id' render={props => {
             const list = lists.find(list => list.id === parseInt(props.match.params.id))
             return <ListCard list={list} {...props}/>
+          }}/>
+          <Route exact path='/lists/:id/edit' render={props => {
+            const list = lists.find(list => list.id === parseInt(props.match.params.id))
+            return <EditListFormWrapper list={list} {...props}/>
           }}/>
         </Switch>
       </div>
