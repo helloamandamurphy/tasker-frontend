2c7c541be6feee52b0874611c6b33af7b7584a8f
Amanda Murphy <helloamandamurphy@gmail.com>
2020-05-31 17:20:38 -0400

    Removed excess code and moved it to LoggedIn.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 //React + Dependencies
 import React from 'react';
 import { connect } from 'react-redux';
-import { Route, Switch, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 
 //Import from Files
 import { getCurrentUser } from "./actions/currentUser"
-import NavBar from "./components/NavBar"
-import MyLists from './components/MyLists'
-import ListCard from './components/ListCard'
-import NewListFormWrapper from './components/NewListFormWrapper'
-import EditListFormWrapper from './components/EditListFormWrapper'
 import LoggedOut from './containers/LoggedOut'
+import LoggedIn from './containers/LoggedIn'
 
 class App extends React.Component {
   componentDidMount() {
@@ -18,22 +14,10 @@ class App extends React.Component {
   }
 
   render() {
-    const { loggedIn, lists } = this.props
+    const { loggedIn } = this.props
     return (
       <div className="App">
-        { loggedIn ? <NavBar/> : <LoggedOut/> }
-        <Switch>
-          <Route exact path='/lists' component={MyLists}/>
-          <Route exact path='/lists/new' component={NewListFormWrapper}/>
-          <Route exact path='/lists/:id' render={props => {
-            const list = lists.find(list => list.id === parseInt(props.match.params.id))
-            return <ListCard list={list} {...props}/>
-          }}/>
-          <Route exact path='/lists/:id/edit' render={props => {
-            const list = lists.find(list => list.id === parseInt(props.match.params.id))
-            return <EditListFormWrapper list={list} {...props}/>
-          }}/>
-        </Switch>
+        { loggedIn ? <LoggedIn/> : <LoggedOut/> }
       </div>
     );
   }
@@ -43,7 +27,6 @@ class App extends React.Component {
 const mapStateToProps = state => {
   return ({
     loggedIn: !!state.currentUser,
-    lists: state.myLists
   })
 }
 
