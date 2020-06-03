7bb195fac635447a2970c8045567b78cd005db56
Amanda Murphy <helloamandamurphy@gmail.com>
2020-06-03 15:48:02 -0400

    Add hardcoded information to the ListCard to show what future development will look like.

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,15 +1,30 @@
 // React + Dependencies
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
 // functional / stateless component
 const ListCard = ({ list }) => {
   return (
     list ?
       <div>
-        <h3>{list.name}</h3>
-        <p>{list.end_time}</p>
+        <h2>{list.name}</h2>
+        <p>Your deadline for this list is <b>{moment(list.end_time).format('LT')}</b></p>
+        <h3>Tasks</h3>
+        <i>In future development, this is where I would add the ability to create, read, update, and delete tasks for each list, along with a timer for each task.</i>
+        <ul>
+          <li>Respond to email, 30 minutes.</li>
+          <li>Work on code, 180 minutes.</li>
+          <li>Write a blog post on React, 60 minutes.</li>
+        </ul>
         <Link to={`/lists/${list.id}/edit`}>Edit this List</Link>
+        <div class="ui hidden divider"></div>
+        <div class="ui center aligned container">
+          <button class="ui huge green button">Start Working</button>
+        </div>
+        <div class="ui hidden divider"></div>
+        <i>In future development, this button would start a timer for the first task. When the time was up, the user would receive an alert to move to the next task.</i>
+        <div class="ui hidden divider"></div>
       </div> :
     <p>Sorry! We couldn't find your list.</p>
   )
