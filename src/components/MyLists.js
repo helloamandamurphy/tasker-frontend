b1a5f9f647a834efe8766c5b3312c7c4414f04db
Amanda Murphy <helloamandamurphy@gmail.com>
2020-06-03 15:08:47 -0400

    Add a note if no lists are found at /lists

--- src/components/MyLists.js
+++ src/components/MyLists.js
@@ -6,7 +6,8 @@ import { Link } from 'react-router-dom'
 // functional / stateless component
 const MyLists = props => {
   const listCards = props.lists.length > 0 ?
-    props.lists.map(l => (<Link to={`/lists/${l.id}`} key={l.id}><button class="ui blue huge button" style={{display: "block", margin: "1em"}}>{l.name}</button></Link>)) : null
+    props.lists.map(l => (<Link to={`/lists/${l.id}`} key={l.id}><button class="ui blue huge button" style={{display: "block", margin: "1em"}}>{l.name}</button></Link>))
+    : <p>You currently have no lists. <Link to="/lists/new">Create a list</Link> to get started!</p>
 
   return listCards
 }
