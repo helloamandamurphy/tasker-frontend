b892ea7e37a87ad9bdff759c88a184df8af7dabd
Amanda Murphy <helloamandamurphy@gmail.com>
2020-06-03 15:46:19 -0400

    Remove superfluous code for clock, and replace it with Moment.js

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,50 +1,18 @@
 // React + Dependencies
 import React from 'react';
+import moment from 'moment';
 
-//Clock from this Codepen: https://codepen.io/tchx/pen/PBjJqW?editors=0010
-let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let months = ['January', 'February', 'March', 'April', 'May', 'June',
-              'July', 'August', 'September', 'October', 'November', 'December'];
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-
-  static pad(num, digit) {
-    let zero = '';
-    for (let i = 0; i < digit; ++i) zero += '0';
-    return (zero+num).slice(-digit);
-  }
-
-  tick() { this.setState({ date: new Date() }); }
-
-  componentDidMount() {
-    this.timer = setInterval(() => this.tick(), 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  render() {
-    let date = this.state.date;
-    let h = Clock.pad(date.getHours(), 2);
-		let m = Clock.pad(date.getMinutes(), 2);
-		// let s = Clock.pad(date.getSeconds(), 2);
-    let d = `${days[date.getDay()]}, ${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
-
+const Clock = () => {
     return (
       <React.Fragment>
         <div class="ui divider"></div>
         <div>
-          <h3 class="ui center aligned header">{d}</h3>
-          <h1 class="ui center aligned huge header" style={{"font-size": "10em"}}>{h}:{m}</h1>
+          <h3 class="ui center aligned header">{moment().format('MMMM Do YYYY')}</h3>
+          <h1 class="ui center aligned huge header" style={{"font-size": "10em"}}>{moment().format('h:mm a')}</h1>
         </div>
         <div class="ui divider"></div>
       </React.Fragment>
     );
   }
-}
 
 export default Clock
